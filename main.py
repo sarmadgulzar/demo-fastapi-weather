4e57d3e2b0baf2e28051556e89ba51cd27880963
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-07-05 00:26:25 +0500

    Added static files into base template

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
 app = FastAPI()
 templates = Jinja2Templates("templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
