ed0312028a26f4ea66484eff148eb8ae4b7cd422
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-07-05 00:20:07 +0500

    Returned template response from index

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates("templates")
 
 
 @app.get("/")
-def index():
-    return {"detail": "Hello World!"}
+def index(request: Request):
+    message = "Hello World!"
+
+    return templates.TemplateResponse(
+        name="index.html",
+        context={
+            "request": request,
+            "message": message,
+        },
+    )
 
 
 if __name__ == "__main__":
