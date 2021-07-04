b18b236fa5c916ca626a034edd234d2c4fbf7841
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-07-05 00:56:16 +0500

    Added pydantic model

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from typing import Optional
+
 import uvicorn
 from fastapi import FastAPI
+from fastapi.params import Depends
+from pydantic import BaseModel
 from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
@@ -23,17 +26,18 @@ def index(request: Request):
     )
 
 
+class Location(BaseModel):
+    city: str
+    state: Optional[str] = None
+    country: str = "US"
+
+
 @app.get("/api/weather")
-def weather(
-    city: str,
-    state: Optional[str] = None,
-    country: str = "US",
-    units: Optional[str] = "metric",
-):
+def weather(location: Location = Depends(), units: Optional[str] = "metric"):
     return {
-        "city": city,
-        "state": state,
-        "country": country,
+        "city": location.city,
+        "state": location.state,
+        "country": location.country,
         "units": units,
     }
 
