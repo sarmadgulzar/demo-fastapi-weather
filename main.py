821a030ab9b5175aec088ee5c14ff2175fd319be
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-07-05 01:46:48 +0500

    Added function to get weather data

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import os
 from typing import Optional
 
+import requests
 import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.params import Depends
 from pydantic import BaseModel
 from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
+
+load_dotenv()
+
+OPENWEATHERMAPS_API_KEY = os.getenv("OPENWEATHERMAPS_API_KEY", None)
 
 app = FastAPI()
 templates = Jinja2Templates("templates")
@@ -32,14 +39,32 @@ class Location(BaseModel):
     country: str = "US"
 
 
+def get_report(
+    city: str,
+    country: str,
+    state: Optional[str] = None,
+    units: Optional[str] = "metric",
+):
+    if state is not None:
+        q = f"{city},{state},{country}"
+    else:
+        q = f"{city},{country}"
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={q}&appid={OPENWEATHERMAPS_API_KEY}&units={units}"
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    data = response.json()
+    forecast = data["main"]
+
+    return forecast
+
+
 @app.get("/api/weather")
 def weather(location: Location = Depends(), units: Optional[str] = "metric"):
-    return {
-        "city": location.city,
-        "state": location.state,
-        "country": location.country,
-        "units": units,
-    }
+    report = get_report(**location.dict(), units=units)
+    return report
 
 
 if __name__ == "__main__":
