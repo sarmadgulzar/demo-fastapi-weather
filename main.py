d899aaff4c1b6c03a79aa76735e215b5a0fbd396
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-09-18 09:46:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 origins = [
     "https://zg9oso.deta.dev/",
     "http://localhost",
-    "http://localhost:8080",
+    "http://localhost:8000",
     "http://127.0.0.1",
-    "http://127.0.0.1:8080",
+    "http://127.0.0.1:8000",
 ]
 
 app.add_middleware(
